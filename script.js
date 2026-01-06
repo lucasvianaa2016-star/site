c1f2f9a4c1f9eb2e7d98397e807579421a56dcdd
lucasvianaa2016-star <lucasvianaa2016@gmail.com>
2026-01-06 01:20:13 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+let score = 0
+let time = 10
+let timer = null
+let record = localStorage.getItem("record") || 0
+
+const btn = document.getElementById("btn")
+const restartBtn = document.getElementById("restart")
+const scoreText = document.getElementById("score")
+const timeText = document.getElementById("time")
+const recordText = document.getElementById("record")
+const clickSound = document.getElementById("clickSound")
+
+recordText.textContent = record
+
+btn.addEventListener("click", () => {
+  if (time === 10) startGame()
+
+  if (time > 0) {
+    score++
+    scoreText.textContent = score
+    if (clickSound) clickSound.play()
+  }
+})
+
+restartBtn.addEventListener("click", resetGame)
+
+function startGame() {
+  timer = setInterval(() => {
+    time--
+    timeText.textContent = time
+
+    if (time === 0) endGame()
+  }, 1000)
+}
+
+function endGame() {
+  clearInterval(timer)
+  btn.disabled = true
+  restartBtn.disabled = false
+  btn.textContent = "FIM DE JOGO"
+
+  if (score > record) {
+    record = score
+    localStorage.setItem("record", record)
+    recordText.textContent = record
+  }
+}
+
+function resetGame() {
+  score = 0
+  time = 10
+  btn.disabled = false
+  restartBtn.disabled = true
+  btn.textContent = "CLICAR"
+
+  scoreText.textContent = score
+  timeText.textContent = time
+}
